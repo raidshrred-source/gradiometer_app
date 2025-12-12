25257223bcbef1437228e83f360f61f1a826bd1c
raidshrred-source <raidshr.red@gmail.com>
2025-12-12 03:03:21 +0100

    Update and rename MainActivity.java to MainActivity.kt

--- android/app/src/main/java/com/example/gradiometer_app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.gradiometer_app; // Make sure this matches your folder name!
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
---
+++ android/app/src/main/java/com/example/gradiometer_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gradiometer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+// Minimal v2 embedding MainActivity
+class MainActivity: FlutterActivity() {
