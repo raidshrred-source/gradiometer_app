b478d8d013de2f01c8d70474498ae9cb23776fa0
raidshrred-source <raidshr.red@gmail.com>
2025-12-12 02:50:26 +0100

    Create MainActivity.java

---
+++ android/app/src/main/java/com/example/gradiometer_app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.gradiometer_app; // Make sure this matches your folder name!
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
